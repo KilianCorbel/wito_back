b1b61255ff6f1f55a435f070f08024d0cc4cf785
Fakih <fakih.darkaoui@hotmail.com>
2019-10-02 19:10:17 +0200

    creation de cours

--- Cours/coursAction.js
+++ Cours/coursAction.js
@@ -1,3 +1,20 @@
 // -- Load model needed for the project
 const CoursProcess = require('./coursProcess');
 let auth = module.exports;
+
+// -- CREATE
+auth.actionCreate = async function(req, res) {
+    console.log("Action : Cours - CREATE");
+
+    try{
+        CoursProcess.processCreate(req).then((callback) => {
+            console.log("Process : Cours - CREATE : " + callback);
+
+            res.send(callback);
+        });
+    } catch(err) {
+        console.log("Process : Cours - CREATE : Error - " + err);
+
+        res.send(err);
+    }
+};
--- Cours/coursProcess.js
+++ Cours/coursProcess.js
@@ -3,3 +3,12 @@ const auth = module.exports;
 const Cours = require("./coursModel");
 
 ObjectId = mongoose.Types.ObjectId;
+
+// -- CREATE
+auth.processCreate = async function(req, mdp) {
+    console.log("Process : Cours - CREATE :" + req.body.nom);
+
+    newCours = new Cours({idCours:req.body.idCours, nom:req.body.nom, heureD:req.body.heureD, heureF:req.body.heureF, date: req.body.date, salle:req.body.salle});
+
+    return await newCours.save();
+};
